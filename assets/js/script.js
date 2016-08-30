0a9e247f7e458c1beca3e4c8f7f82e20e1ac0444
Saswata Gupta <gupta.saswata@gmail.com>
2016-08-30 16:55:12 -0400

    Added logo on navbar and fiel to display text on editable sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var addActive = function(section){
+	$('#navbar-items li[data-id]').each(function(index){
+		$(this).removeClass("active");
+	});	
+
+	if (section) $(section+"-btn").addClass('active');
+}
+
+var scrollToID = function(id, speed){
+	var offset = 70;
+	var targetOffset = $(id).offset().top - offset;	
+	if (id==="#about") targetOffset -= 250;	
+
+	//addActive(id);
+
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+}
+
 var newArt = function(anim){
 
 	rand = Math.ceil(Math.random()*28);		
@@ -19,6 +37,82 @@ var newArt = function(anim){
 }
 
 
-$(function(){	
+$(function(){
+	$('*[data-text]').each(function(index){
+		$(this).html(data[$(this).attr('data-text')]);
+	});
+
+	//Add projects
+	
+	$('.carousel-indicators').empty();
+	$('.carousel-inner').empty();
+
+	for (i in data.projects){
+		var proj = data.projects[i];
+		var ind = i.toString();
+		var active = i==0 ? 'active':'';		
+
+		$('<li/>',{			
+			"data-target": "#projects",
+			"data-slide-to": ind,
+			"class": active,
+		}).appendTo(".carousel-indicators");
+
+
+
+		$('<div/>', {
+			"id": "project"+ind,
+			"class": "item "+active,
+		}).appendTo(".carousel-inner");
+
+
+
+		$('<img>', {
+			"src": proj.img,
+		}).appendTo('#project'+ind);
+
+		$('<div/>', {
+			"id": "caption"+ind,
+			"class": "carousel-caption",
+		}).appendTo('#project'+ind);
+
+
+
+		$('<h3/>',{
+			"text": proj.name,
+		}).appendTo("#caption"+ind);
+
+		$('<p/>',{
+			"text": proj.description,
+		}).appendTo("#caption"+ind);
+
+		$('<a/>',{
+			"class" : "btn btn-project btn-lg",
+			"href" : proj.link,
+			"target" : "_blank",
+			"text": "View",
+		}).appendTo("#caption"+ind);
+	}
+	
+
+	addActive("#intro");
 	newArt(false);	
+
+	$('#navbar-items li[data-id]').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr('data-id');
+		scrollToID(sectionID, 750);
+	});
+
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		if (scroll<200)
+			addActive("#intro");
+		else if(scroll<780)
+			addActive('#about');
+		else if(scroll<1775)
+			addActive('#projects');
+		else
+			addActive('#contact');		
+	});
 });
---
+++ assets/text/data.js
@@ -0,0 +1,28 @@
+var data = {
+	about: "HEY ITS ME!",
+
+	email: "saswata.gupta@uwaterloo.ca",
+
+	projects: [
+		{
+			name: "libestimote",
+			description: "libraries and stuff",
+			link: "https://github.com/sas5580/LIBestimote",
+			img: "assets/img/book-bg.jpg",
+		},
+
+		{
+			name:"myusicality",
+			description: "music and myo stuff",			
+			link: "https://github.com/NathanJiangCS/MasseyHacks2",
+			img: "assets/img/bulb-bg.jpg",
+		},
+		
+		{
+			name: "abstract art generator",
+			description: "art and class",
+			link: "https://github.com/sas5580/Abstract-Art-Generator",
+			img: "assets/img/art-bg.jpg",
+		},
+	],
+}
